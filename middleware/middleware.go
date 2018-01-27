dd3f6da8dc3024b9e38a3c4fa202f171e4a98a98
skrelan <skrelan@gmail.com>
2018-01-26 20:03:03 -0800

    abstracted logic out from m middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -30,11 +29,11 @@ func GetUsers(w http.ResponseWriter, req *http.Request) {
 
 	limit := params.Get("limit")
 	if len(limit) == 0 {
-		limit = "100"
+		limit = LIMIT
 	}
 	offset := params.Get("offset")
 	if len(offset) == 0 {
-		offset = "0"
+		offset = OFFSET
 	}
 	if len(ids) > 0 {
 		res, err = db.GetUserByIDs(&ids)
@@ -65,52 +64,24 @@ func AddRestaurant(w http.ResponseWriter, req *http.Request) {
 func GetRestaurants(w http.ResponseWriter, req *http.Request) {
 	var res *[]models.Restaurant
 	var err error
-	clauses := make([]string, 0, 0)
+
 	var whereClause string
 
 	params := req.URL.Query()
-	ids := params.Get("ids")
-	if len(ids) > 0 {
-		temp := fmt.Sprintf("v.id in (%s)", ids)
-		clauses = append(clauses, temp)
-	}
-	city := params.Get("city")
-	if len(city) > 0 {
-		temp := fmt.Sprintf("LOWER(v.city) in LOWER('%s')", city)
-		clauses = append(clauses, temp)
-	}
-	zipcode := params.Get("zip_code")
-	if len(zipcode) > 0 {
-		temp := fmt.Sprintf("v.zip_code in ('%s')", zipcode)
-		clauses = append(clauses, temp)
-	}
-	category := params.Get("category")
-	if len(category) > 0 {
-		temp := fmt.Sprintf("LOWER(r.category) in LOWER('%s')", category)
-		clauses = append(clauses, temp)
-	}
-	// totalscore := params.get("totalscore")
-	// if len(ids){
-	// 	temp := fmt.Sprintf("v.city in (%s)", cities)
-	// 	clauses = append(clauses,temp)
-	// }
-	name := params.Get("name")
-	if len(name) > 0 {
-		temp := fmt.Sprintf("LOWER(r.name) = LOWER('%s')", name)
-		clauses = append(clauses, temp)
-	}
+
+	clauses := restaurantFilters(&params)
 
 	limit := params.Get("limit")
 	if len(limit) == 0 {
-		limit = "100"
+		limit = LIMIT
 	}
 	offset := params.Get("offset")
 	if len(offset) == 0 {
-		offset = "0"
+		offset = OFFSET
 	}
 
-	if len(clauses) > 0 {
-		whereClause = strings.Join(clauses, " AND ")
+	if len(*clauses) > 0 {
+		whereClause = strings.Join(*clauses, " AND ")
 		res, err = db.GetVenuesWhere(&whereClause, &limit, &offset)
 		if err != nil {
 			log.Error(err)
@@ -140,11 +111,11 @@ func GetRatings(w http.ResponseWriter, req *http.Request) {
 	params := req.URL.Query()
 	limit := params.Get("limit")
 	if len(limit) == 0 {
-		limit = "100"
+		limit = LIMIT
 	}
 	offset := params.Get("offset")
 	if len(offset) == 0 {
-		offset = "0"
+		offset = OFFSET
 	}
 
 	res, err := db.GetAllRatings(&limit, &offset)
